62f5f010721da7bfb49409b660bffe6c8997d64a
Raptor Repack <118311535+RaptorRepack@users.noreply.github.com>
2023-08-21 11:38:44 +0600

    Add files via upload

---
+++ Toolkit/Stable Project/v2.2.0/Tools/Location/Clean.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d "d:\RepackToolkit\Tools\Compressor\Files\Data"
+cls
+title delete files and fodler
+echo deleting...
+echo.
+echo please wait!
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+del . /q 
+exit
+rem the "del" command is useful for deleting files
+rem the "*.*" argument on the del command is useful for deleting every types of file
+rem this command: for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q) 
+rem is useful for deleting every files in a directory
